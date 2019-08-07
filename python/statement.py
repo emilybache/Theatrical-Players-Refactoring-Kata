cd583380f51b0ddbc4de5a4ff8dd57cc50899ada
Emily Bache <emily.bache@praqma.com>
2019-08-07 08:47:07 +0200

    python version of exercise

---
+++ python/statement.py
@@ -0,0 +1,41 @@
+import math
+
+
+def statement(invoice, plays):
+    total_amount = 0
+    volume_credits = 0
+    result = f'Statement for {invoice["customer"]}\n'
+
+    def format_as_dollars(amount):
+        return f"${amount:0,.2f}"
+
+    for perf in invoice['performances']:
+        play = plays[perf['playID']]
+        if play['type'] == "tragedy":
+            this_amount = 40000
+            if perf['audience'] > 30:
+                this_amount += 1000 * (perf['audience'] - 30)
+        elif play['type'] == "comedy":
+            this_amount = 30000
+            if perf['audience'] > 20:
+                this_amount += 10000 + 500 * (perf['audience'] - 20)
+
+            this_amount += 300 * perf['audience']
+
+        else:
+            raise ValueError(f'unknown type: {play["type"]}')
+
+        # add volume credits
+        volume_credits += max(perf['audience'] - 30, 0)
+        # add extra credit for every ten comedy attendees
+        if "comedy" == play["type"]:
+            volume_credits += math.floor(perf['audience'] / 5)
+        # print line for this order
+        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
+        total_amount += this_amount
+
+    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
+    result += f'You earned {volume_credits} credits\n'
+    return result
+
+
---
+++ python/tests/test_statement.py
@@ -0,0 +1,25 @@
+import json
+
+import pytest
+from approvaltests import verify
+from approvaltests.utils import get_adjacent_file
+
+from statement import statement
+
+
+def test_example_statement():
+    with open(get_adjacent_file("invoice.json")) as f:
+        invoice = json.loads(f.read())
+    with open(get_adjacent_file("plays.json")) as f:
+        plays = json.loads(f.read())
+    verify(statement(invoice, plays))
+
+
+def test_statement_with_new_play_types():
+    with open(get_adjacent_file("invoice_new_plays.json")) as f:
+        invoice = json.loads(f.read())
+    with open(get_adjacent_file("new_plays.json")) as f:
+        plays = json.loads(f.read())
+    with pytest.raises(ValueError) as exception_info:
+        statement(invoice, plays)
+    assert "unknown type" in str(exception_info.value)
