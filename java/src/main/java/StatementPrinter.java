f20b30d0b89dbd97b43364d841c23b763b31b14e
Emily Bache <emily.bache@praqma.com>
2019-08-13 09:38:05 +0200

    updated java version so it matches javascript version of tests

--- java/src/main/java/StatementPrinter.java
+++ java/src/main/java/StatementPrinter.java
@@ -36,7 +36,7 @@ public class StatementPrinter {
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
-            if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+            if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
             // print line for this order
             result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
