438c09d8ceacf3d82390a5eea9722ce19b6c127e
Tommy Tho <tommythowm@gmail.com>
2022-05-19 11:20:30 -0700

    reduced nested referencing

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,36 +1,39 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  const customer = invoice.customer;
+  let performances = invoice.performances;
+  let result = `Statement for ${customer}\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of performances) {
+    const audience = perf.audience;
     const play = plays[perf.playID];
     let thisAmount = 0;
     switch (play.type) {
       case "tragedy":
         thisAmount = 40000;
         if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+          thisAmount += 1000 * (audience - 30);
         }
         break;
       case "comedy":
         thisAmount = 30000;
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+          thisAmount += 10000 + 500 * (audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        thisAmount += 300 * audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
     // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === play.type) volumeCredits += Math.floor(audience / 5);
     // print line for this order
-    result += ` ${play.name}: ${formatCurrency(thisAmount / 100)} (${
-      perf.audience
-    } seats)\n`;
+    result += ` ${play.name}: ${formatCurrency(
+      thisAmount / 100
+    )} (${audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
