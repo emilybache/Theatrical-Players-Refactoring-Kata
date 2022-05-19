1f5ae39243f67590f29c72b91a01a6827ea51b06
Tommy Tho <tommythowm@gmail.com>
2022-05-19 12:03:57 -0700

    moved mocks to a folder

--- javascript/test/statement.test.js
+++ javascript/test/statement.test.js
@@ -1,14 +1,22 @@
-const statement = require('../src/statement');
-const fs=require('fs');
+const statement = require("../src/statement");
+const fs = require("fs");
 
-test('example statement', () => {
-    const invoice = JSON.parse(fs.readFileSync('test/invoice.json', 'utf8'));
-    const plays = JSON.parse(fs.readFileSync('test/plays.json', 'utf8'));
-    expect(statement(invoice, plays)).toMatchSnapshot();
+test("example statement", () => {
+  const invoice = JSON.parse(
+    fs.readFileSync("test/mocks/invoice.json", "utf8")
+  );
+  const plays = JSON.parse(fs.readFileSync("test/mocks/plays.json", "utf8"));
+  expect(statement(invoice, plays)).toMatchSnapshot();
 });
 
-test('statement with new play types', () => {
-    const invoice = JSON.parse(fs.readFileSync('test/invoice_new_plays.json', 'utf8'));
-    const plays = JSON.parse(fs.readFileSync('test/new_plays.json', 'utf8'));
-    expect(() => {statement(invoice, plays)}).toThrow(/unknown type/);
+test("statement with new play types", () => {
+  const invoice = JSON.parse(
+    fs.readFileSync("test/mocks/invoice_new_plays.json", "utf8")
+  );
+  const plays = JSON.parse(
+    fs.readFileSync("test/mocks/new_plays.json", "utf8")
+  );
+  expect(() => {
+    statement(invoice, plays);
+  }).toThrow(/unknown type/);
 });
