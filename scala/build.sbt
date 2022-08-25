527bd42a9a6f5c9b15fd5712e119c447e453c3bf
Yoan Thirion <yoan.thirion@gmail.com>
2022-08-25 10:32:29 +0200

    add writethemfirst library for approval testing

--- scala/build.sbt
+++ scala/build.sbt
@@ -1,6 +1,7 @@
 name := "TheatricalPlayersRefactoringKata"
 
 version := "1.0"
-scalaVersion := "2.11.12"
+scalaVersion := "2.13.8"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.11" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.12" % "test"
+libraryDependencies += "com.github.writethemfirst" %% "approvals-scala" % "0.12.2" % "test"
--- scala/src/test/scala/StatementPrinterTests.scala
+++ scala/src/test/scala/StatementPrinterTests.scala
@@ -1,3 +1,5 @@
+import com.github.writethemfirst.approvals.Approvals
+import com.github.writethemfirst.approvals.Approvals._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -21,13 +23,7 @@ class StatementPrinterTests extends AnyFunSuite with Matchers {
     val statementPrinter = new StatementPrinter()
     val result = statementPrinter.print(invoice, plays)
 
-    result should be("""Statement for BigCo
-        |  Hamlet: $650.00 (55 seats)
-        |  As You Like It: $580.00 (35 seats)
-        |  Othello: $500.00 (40 seats)
-        |Amount owed is $1,730.00
-        |You earned 47 credits
-        |""".stripMargin)
+    verify(result)
   }
 
   test("statement with new play types") {
