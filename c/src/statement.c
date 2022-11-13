48a988870d7ad463fdbf06801d011191fa396e2e
Nitsan Avni <nitsanav@gmail.com>
2022-11-13 15:16:22 +0100

    currency number format

--- c/src/statement.c
+++ c/src/statement.c
@@ -4,49 +4,39 @@
 #include <string.h>
 #include <stdio.h>
 
-#define the_max(x,y) (((x) >= (y)) ? (x) : (y))
+#define the_max(x, y) (((x) >= (y)) ? (x) : (y))
 
-void statement(char* result, struct Invoice *invoice, int numberOfPerformances,
-        struct Play **plays, int numberOfPlays) {
+void statement(char *result, struct Invoice *invoice, int numberOfPerformances,
+               struct Play **plays, int numberOfPlays) {
     float total_amount = 0;
     int volume_credits = 0;
     float this_amount = 0;
-    struct Play* play;
+    struct Play *play;
 
     sprintf(result, "Statement for %s\n", invoice->customer);
 
-    for(int i = 0; i < numberOfPerformances ; i++)
-    {
+    for (int i = 0; i < numberOfPerformances; i++) {
         this_amount = 0;
-        for (int j = 0; j < numberOfPlays; j++)
-        {
+        for (int j = 0; j < numberOfPlays; j++) {
             if (strcmp(plays[j]->playID, invoice->performances[i]->playID) == 0) {
                 play = plays[j];
             }
         }
-        if (strcmp(play->type, "tragedy") == 0)
-        {
+        if (strcmp(play->type, "tragedy") == 0) {
             this_amount = 40000;
-            if (invoice->performances[i]->audience > 30)
-            {
+            if (invoice->performances[i]->audience > 30) {
                 this_amount += 1000 * (invoice->performances[i]->audience - 30);
             }
-        }
-
-        else if (strcmp(play->type, "comedy") == 0)
-        {
+        } else if (strcmp(play->type, "comedy") == 0) {
             this_amount = 30000;
-            if (invoice->performances[i]->audience > 20)
-            {
+            if (invoice->performances[i]->audience > 20) {
                 this_amount += 10000 + 500 * (invoice->performances[i]->audience - 20);
             }
 
             this_amount += 300 * invoice->performances[i]->audience;
-        }
-
-        else
-        {
-            sprintf(result, "ERROR: unknown play type %s", play->type);
+        } else {
+            sprintf(result, "ERROR: unknown play type %s\n", play->type);
+            printf("ERROR: unknown play type %s", play->type);
             return;
         }
 
@@ -54,30 +44,32 @@ void statement(char* result, struct Invoice *invoice, int numberOfPerformances,
         volume_credits += the_max(invoice->performances[i]->audience - 30, 0);
 
         // add extra credit for every ten comedy attendees
-        if (strcmp(play->type, "comedy") == 0)
-        {
+        if (strcmp(play->type, "comedy") == 0) {
             volume_credits += invoice->performances[i]->audience / 5;
         }
 
         // print line for this order
-        sprintf(result, "%s %s: $%.2f (%d seats)\n", result, play->name, (this_amount/100), invoice->performances[i]->audience);
+        sprintf(result, "%s %s: $%s (%d seats)\n", result, play->name,
+                format_currency_number(this_amount / 100),
+                invoice->performances[i]->audience);
         total_amount += this_amount;
     }
 
-    sprintf(result, "%sAmount owed is $%.2f\n", result, (total_amount/100));
+    sprintf(result, "%sAmount owed is $%s\n", result,
+            format_currency_number(total_amount / 100));
     sprintf(result, "%sYou earned %d credits", result, volume_credits);
 }
 
-struct Invoice *Invoice_create(char* customer, struct Performance** performances) {
-    struct Invoice* invoice = malloc(sizeof(struct Invoice));
+struct Invoice *Invoice_create(char *customer, struct Performance **performances) {
+    struct Invoice *invoice = malloc(sizeof(struct Invoice));
     strncpy(invoice->customer, customer, sizeof(invoice->customer) - 1);
     memcpy(invoice->performances, performances, sizeof(invoice->performances));
 
     return invoice;
 }
 
-struct Play *Play_create(char* playID, char *name, char *type) {
-    struct Play* play = malloc(sizeof(struct Play));
+struct Play *Play_create(char *playID, char *name, char *type) {
+    struct Play *play = malloc(sizeof(struct Play));
     strncpy(play->playID, playID, sizeof(play->playID) - 1);
     strncpy(play->name, name, sizeof(play->name) - 1);
     strncpy(play->type, type, sizeof(play->type) - 1);
@@ -85,9 +77,35 @@ struct Play *Play_create(char* playID, char *name, char *type) {
 }
 
 struct Performance *Performance_create(char *playID, int audience) {
-    struct Performance* performance = malloc(sizeof(struct Performance));
+    struct Performance *performance = malloc(sizeof(struct Performance));
     strncpy(performance->playID, playID, sizeof(performance->playID) - 1);
     performance->audience = audience;
     return performance;
 }
 
+void commafy(char *buffer, double n) {
+    if (n < 1000) {
+        sprintf(buffer, "%.2f", n);
+        return;
+    }
+
+    long floor = (long) n;
+    double smallest_part = (n - (double) floor) + (double) (floor % 1000);
+
+    if (n < 1000 * 1000) {
+        long d = floor / 1000;
+        sprintf(buffer, "%ld,%06.2f", d, smallest_part);
+    } else if (n < 1000 * 1000 * 1000) {
+        long d1 = floor / (1000 * 1000);
+        long d2 = (floor / 1000) % 1000;
+        sprintf(buffer, "%ld,%03ld,%06.2f", d1, d2, smallest_part);
+    }
+}
+
+char *format_currency_number(double number) {
+    static char buffer[200];
+    buffer[0] = '\0';
+    commafy(buffer, number);
+
+    return buffer;
+}
--- c/test-catch2/sample_catch.cpp
+++ c/test-catch2/sample_catch.cpp
@@ -10,32 +10,33 @@ extern "C"
 #define MAX_PRINT_LENGTH 20000
 
 TEST_CASE ("Theatrical Players") {
-    struct Play* hamlet = Play_create("hamlet", "Hamlet", "tragedy");
-    struct Play* aslike = Play_create("as-like", "As You Like It", "comedy");
-    struct Play* othello = Play_create("othello", "Othello", "tragedy");
+    struct Play *hamlet = Play_create("hamlet", "Hamlet", "tragedy");
+    struct Play *aslike = Play_create("as-like", "As You Like It", "comedy");
+    struct Play *othello = Play_create("othello", "Othello", "tragedy");
     char result[MAX_PRINT_LENGTH];
 
     SECTION("existing play types") {
-        struct Performance* bigCoHamlet = Performance_create("hamlet", 55);
-        struct Performance* bigCoAsLike = Performance_create("as-like", 35);
-        struct Performance* bigCoOthello = Performance_create("othello", 40);
+        struct Performance *bigCoHamlet = Performance_create("hamlet", 55);
+        struct Performance *bigCoAsLike = Performance_create("as-like", 35);
+        struct Performance *bigCoOthello = Performance_create("othello", 40);
 
-        Performance* performances[] = {bigCoHamlet, bigCoAsLike, bigCoOthello};
-        struct Invoice* invoice = Invoice_create("BigCo", performances);
-        Play* plays[] = { hamlet, aslike, othello };
+        Performance *performances[] = {bigCoHamlet, bigCoAsLike, bigCoOthello};
+        struct Invoice *invoice = Invoice_create("BigCo", performances);
+        Play *plays[] = {hamlet, aslike, othello};
 
         statement(result, invoice, 3, plays, 3);
 
         ApprovalTests::Approvals::verify(std::string(result));
     }
-    SECTION("new play types") {
-        struct Play* henryv = Play_create("henry-v", "Henry V", "history");
-        Play* plays[] = { aslike, henryv };
-        struct Performance* bigCoIIAsLike = Performance_create("as-like", 55);
-        struct Performance* bigCoIIHenryV = Performance_create("henry-v", 53);
 
-        Performance* performances[] = {bigCoIIAsLike, bigCoIIHenryV};
-        struct Invoice* invoice = Invoice_create("BigCoII", performances);
+    SECTION("new play types") {
+        struct Play *henryv = Play_create("henry-v", "Henry V", "history");
+        Play *plays[] = {aslike, henryv};
+        struct Performance *bigCoIIAsLike = Performance_create("as-like", 55);
+        struct Performance *bigCoIIHenryV = Performance_create("henry-v", 53);
+
+        Performance *performances[] = {bigCoIIAsLike, bigCoIIHenryV};
+        struct Invoice *invoice = Invoice_create("BigCoII", performances);
 
         statement(result, invoice, 2, plays, 2);
 
@@ -43,4 +44,17 @@ TEST_CASE ("Theatrical Players") {
     }
 }
 
-
+TEST_CASE ("Currency Number Format") {
+    SECTION("existing play types") {
+        ApprovalTests::Approvals::verifyAll("format_currency_number",
+                                            {1.0, 1.9, 1.99, 1000.0, 999.99, 1001.23,
+                                             1001001.01,
+                                             10001.1, 10000.0, 7500.0, 1234567.89,
+                                             12345678.9},
+                                            [](double input, std::ostream &stream) {
+                                                stream << input << " => "
+                                                       << format_currency_number(input)
+                                                       << "\n";
+                                            });
+    }
+}
