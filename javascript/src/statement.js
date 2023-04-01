117e641c71b6d3a8a0c8dd554c335a35a17ae952
woong9149 <sewoong0409@naver.com>
2023-04-01 19:53:18 +0900

    [Refactor] 역할 분리
    - 연산 로직과 출력 로직 분리

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,10 +1,11 @@
+
+
 function getAmountByPlayType(playType, audience) {
     switch (playType) {
         case "tragedy":
             return 40000 + (audience > 30 ? 1000 * (audience - 30) : 0)
         case "comedy":
             return 30000 + (300 * audience) + (audience > 20 ? 10000 + 500 * (audience - 20) : 0);
-
         default:
             throw new Error(`unknown type: ${playType}`);
     }
@@ -22,25 +23,37 @@ function USDFormatting(_number) {
       }).format(_number);
 }
 
-function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
+function createResultStatement(_invoice, _plays, _calculatedObject) {
+    let result = `Statement for ${_invoice.customer}\n`;
+    result += createPlayDescriptionStatement(_invoice.performances, _plays);
+    result += `Amount owed is ${USDFormatting(_calculatedObject.totalAmount/100)}\n`;
+    result += `You earned ${_calculatedObject.volumeCredits} credits\n`;
+    return result;
+}
 
+function createPlayDescriptionStatement(performances, plays) {
+    let playDescriptionStatement = '';
+    for (let performance of performances) {
+        const play = plays[performance.playID];
+        playDescriptionStatement += ` ${play.name}: ${USDFormatting(getAmountByPlayType(play.type, performance.audience) / 100)} (${performance.audience} seats)\n`;
+    }
+    return playDescriptionStatement;
+}
+
+function createCalculatedObject(invoice, plays) {
+    /* Block scoped variables cannot share name with 'var' variables or parameters in the same block scope */
+    let totalAmount = 0;
+    let volumeCredits = 0
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        thisAmount = getAmountByPlayType(play.type, perf.audience);
-        // add volume credits
         volumeCredits += getVolumeCredits(play.type, perf.audience);
-        // print line for this order
-        result += ` ${play.name}: ${USDFormatting(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        totalAmount += getAmountByPlayType(play.type, perf.audience);
     }
+    return {volumeCredits, totalAmount};
+}
 
-    result += `Amount owed is ${USDFormatting(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-    return result;
+function statement (invoice, plays) {
+    return createResultStatement(invoice, plays, createCalculatedObject(invoice, plays));
 }
 
 module.exports = statement;
