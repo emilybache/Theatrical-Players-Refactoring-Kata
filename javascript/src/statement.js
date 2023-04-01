84baf9b1a5a406c89624a4d86549c194d8228a2c
woong9149 <sewoong0409@naver.com>
2023-04-01 18:46:56 +0900

    [Refactor] 함수로 추출하기(조건문, 연산 로직)
    - switch~case -> getAmountByPlayType()
    - volumecredis 구하는 함수 따로 분리
    - 포맷팅 함수 분리

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,41 +1,44 @@
+function getAmountByPlayType(playType, audience) {
+    switch (playType) {
+        case "tragedy":
+            return 40000 + (audience > 30 ? 1000 * (audience - 30) : 0)
+        case "comedy":
+            return 30000 + (300 * audience) + (audience > 20 ? 10000 + 500 * (audience - 20) : 0);
+
+        default:
+            throw new Error(`unknown type: ${playType}`);
+    }
+}
+
+function getVolumeCredits(playType, audience) {
+    return Math.max(audience - 30, 0) + ("comedy" === playType ? Math.floor(audience / 5) : 0);
+}
+
+function USDFormatting(_number) {
+    return new Intl.NumberFormat("en-US",
+      {
+          style: "currency", currency: "USD",
+          minimumFractionDigits: 2
+      }).format(_number);
+}
 
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
         let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        thisAmount = getAmountByPlayType(play.type, perf.audience);
         // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += getVolumeCredits(play.type, perf.audience);
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += ` ${play.name}: ${USDFormatting(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+
+    result += `Amount owed is ${USDFormatting(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
