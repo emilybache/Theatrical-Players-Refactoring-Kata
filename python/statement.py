a86f13be2f2a427a00ccaa72473114e4ba85b8bb
My Name <myEmail@example.com>
2023-05-01 18:00:26 +0300

    Первое задание(Theatrical)

--- python/statement.py
+++ python/statement.py
@@ -1,5 +1,13 @@
 import math
 
+EQUAL_FUNC2 = 20
+EQUAL_FUNC = 30
+HANDR = 100
+THREETHOUS = 30000
+FOURTHOUS = 40000
+
+COMEDY = "comedy"
+TRAGEDY = "tragedy"
 
 def statement(invoice, plays):
     total_amount = 0
@@ -11,31 +19,43 @@ def statement(invoice, plays):
 
     for perf in invoice['performances']:
         play = plays[perf['playID']]
-        if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
-
+        if play['type'] == TRAGEDY:
+            this_amount = FOURTHOUS
+            this_amount = AUDIENCE_FUNC(perf, this_amount)
+        elif play['type'] == COMEDY:
+            this_amount = THREETHOUS
+            this_amount = AUDIENCE_FUNC2(perf, this_amount)
             this_amount += 300 * perf['audience']
-
         else:
             raise ValueError(f'unknown type: {play["type"]}')
 
-        # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
-        # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
-        # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
+        volume_credits += max(perf['audience'] - EQUAL_FUNC, 0)
+
+        volume_credits = GET_COMEDY(perf, play, volume_credits)
+
+        result += f' {play["name"]}: {format_as_dollars(this_amount / HANDR)} ({perf["audience"]} seats)\n'
         total_amount += this_amount
 
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
+    result += f'Amount owed is {format_as_dollars(total_amount / HANDR)}\n'
     result += f'You earned {volume_credits} credits\n'
     return result
+
+
+def AUDIENCE_FUNC2(perf, this_amount):
+    if perf['audience'] > EQUAL_FUNC2:
+        this_amount += 10000 + 500 * (perf['audience'] - EQUAL_FUNC2)
+    return this_amount
+
+
+def AUDIENCE_FUNC(perf, this_amount):
+    if perf['audience'] > EQUAL_FUNC:
+        this_amount += 1000 * (perf['audience'] - EQUAL_FUNC)
+    return this_amount
+
+
+def GET_COMEDY(perf, play, volume_credits):
+    if COMEDY == play["type"]:
+        volume_credits += math.floor(perf['audience'] / 5)
+    return volume_credits
 
 
