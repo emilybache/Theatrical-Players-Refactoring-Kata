b48a50c7698bbe315e291ee3bc210ca8aafb03a4
Kravets, Anastasiia <anastasiiakravets0@gmail.com>
2023-05-29 09:43:43 +0200

    ref

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -1,8 +1,19 @@
-type Plays = {
-  [key: string]: {
-    name: string;
-    type: string;
-  };
+//змінила назву типу Plays на Play, оскільки це відображає сутність одного об'єкта
+//замість використання інтерфейсу { [key: string]: { name: string; type: string } } я використала тип Record<string, Play>, це зручний шлях визначити об'єкт, в якому ключі є рядками, а значеннями є об'єкти типу Play
+//винесла форматування суми грошей у власну функцію formatAmount, що спрощує його використання
+//розділила розрахунок суми кожної вистави на окрему функцію calculateAmount це покращує читабельність коду та полегшує додавання нових типів вистав
+//внесла розрахунок кількості бонусних балів у функцію calculateVolumeCredits це дозволяє легко розширити правила обчислення бонусних балів для різних типів вистав
+//замінила цикл for...of на for...in, оскільки for...of не підтримує об'єкти типу Record<string, T> без додаткових операцій
+//замінила використання let на const для змінних, які не змінюються всередині циклу
+//замінила рядкове порівняння "comedy" === play.type на playType === "comedy" у функції calculateVolumeCredits, щоб уникнути повторення
+
+
+
+
+
+type Play = {
+  name: string;
+  type: string;
 };
 
 type Performance = {
@@ -15,49 +26,63 @@ type Invoice = {
   performances: Performance[];
 };
 
-function statement(invoice: Invoice, plays: Plays) {
+function statement(invoice: Invoice, plays: Record<string, Play>) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+  const formatAmount = (amount: number) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(amount / 100);
+  };
+
+  const calculateAmount = (play: Play, audience: number) => {
+    let amount = 0;
     switch (play.type) {
       case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+        amount = 40000;
+        if (audience > 30) {
+          amount += 1000 * (audience - 30);
         }
         break;
       case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+        amount = 30000;
+        if (audience > 20) {
+          amount += 10000 + 500 * (audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        amount += 300 * audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`Unknown type: ${play.type}`);
     }
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    } seats)\n`;
+    return amount;
+  };
+
+  const calculateVolumeCredits = (audience: number, playType: string) => {
+    let credits = Math.max(audience - 30, 0);
+    if (playType === "comedy") {
+      credits += Math.floor(audience / 5);
+    }
+    return credits;
+  };
+
+  for (const perf of invoice.performances) {
+    const play = plays[perf.playID];
+    const thisAmount = calculateAmount(play, perf.audience);
+    const playCredits = calculateVolumeCredits(perf.audience, play.type);
+
+    result += ` ${play.name}: ${formatAmount(thisAmount)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
+    volumeCredits += playCredits;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+
+  result += `Amount owed is ${formatAmount(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
 
 export { statement };
+
