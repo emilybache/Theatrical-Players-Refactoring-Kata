cb9ec30e12a5fb4f6fb146b590872f5059ba2b0e
emily <emily@bacheconsulting.com>
2024-04-12 15:29:11 +0200

    R split loop to put volumeCredits in own loop

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -5,6 +5,8 @@ function statement (invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
+    }
+    for (let perf of invoice.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
