ed80a6fc3f9e37db8d45e4764db9611f9039971a
emily <emily@bacheconsulting.com>
2024-04-12 16:04:06 +0200

    R use customer from data instead of invoice

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,12 +1,13 @@
 
 function statement (invoice, plays) {
     const statementData = {};
+    statementData.customer = invoice.customer;
     return renderPlainText(statementData, invoice, plays);
 }
 
 
 function renderPlainText(data, invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
+    let result = `Statement for ${data.customer}\n`;
     for (let perf of invoice.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
