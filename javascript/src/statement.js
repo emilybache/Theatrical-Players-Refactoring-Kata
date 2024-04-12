ca45a8c6e946ff0775a35df54942d6ebe7ada922
emily <emily@bacheconsulting.com>
2024-04-12 14:57:40 +0200

    r rename variable

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -23,21 +23,21 @@ function statement (invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
-    function amountFor(play, perf) {
+    function amountFor(play, aPerformance) {
         let thisAmount = 0;
         switch (play.type) {
             case "tragedy":
                 thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
+                if (aPerformance.audience > 30) {
+                    thisAmount += 1000 * (aPerformance.audience - 30);
                 }
                 break;
             case "comedy":
                 thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                if (aPerformance.audience > 20) {
+                    thisAmount += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                thisAmount += 300 * perf.audience;
+                thisAmount += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
