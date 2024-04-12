1f5efea1f57156af9a2d9435974e9636bcbb659e
emily <emily@bacheconsulting.com>
2024-04-12 15:26:05 +0200

    R convert function variable to declared function

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -3,10 +3,11 @@ function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
+    function format(aNumber) {
+        return new Intl.NumberFormat("en-US",
         { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
-
+            minimumFractionDigits: 2 }).format(aNumber);
+    }
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
