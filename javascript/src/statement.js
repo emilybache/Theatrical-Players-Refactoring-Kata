c3116048687b5acda3ea8361f1c1cea1e89596e9
emily <emily@bacheconsulting.com>
2024-04-12 16:05:55 +0200

    r remove invoice from parameters as it is unused

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -3,11 +3,11 @@ function statement (invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances;
-    return renderPlainText(statementData, invoice, plays);
+    return renderPlainText(statementData, plays);
 }
 
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         // print line for this order
