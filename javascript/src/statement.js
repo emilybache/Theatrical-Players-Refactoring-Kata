75acee55359f9fdaa74b7ca5c180dd06443a820c
emily <emily@bacheconsulting.com>
2024-04-12 15:30:54 +0200

    r inline totalVolumeCredits

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -3,15 +3,13 @@ function statement (invoice, plays) {
     let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
-    let volumeCredits = totalVolumeCredits();
-
     for (let perf of invoice.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
 
     function amountFor(aPerformance) {
