447b405ec23de3a859223739fef1d0573a3acfa2
emily <emily@bacheconsulting.com>
2024-04-12 16:02:12 +0200

    R change signature to add statementData

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,10 +1,11 @@
 
 function statement (invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    return renderPlainText(statementData, invoice, plays);
 }
 
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(statementData, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // print line for this order
