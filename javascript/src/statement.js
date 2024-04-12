047b35d2fc3b1256f2fdc82924a2f543cfe691b9
emily <emily@bacheconsulting.com>
2024-04-12 14:57:54 +0200

    r rename variable

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -24,25 +24,25 @@ function statement (invoice, plays) {
     return result;
 
     function amountFor(play, aPerformance) {
-        let thisAmount = 0;
+        let result = 0;
         switch (play.type) {
             case "tragedy":
-                thisAmount = 40000;
+                result = 40000;
                 if (aPerformance.audience > 30) {
-                    thisAmount += 1000 * (aPerformance.audience - 30);
+                    result += 1000 * (aPerformance.audience - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (aPerformance.audience > 20) {
-                    thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                thisAmount += 300 * aPerformance.audience;
+                result += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
         }
-        return thisAmount;
+        return result;
     }
 
 }
