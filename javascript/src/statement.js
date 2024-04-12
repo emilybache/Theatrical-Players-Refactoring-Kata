4a5ba12ee76905893acceffd8570af7d7eeb2244
emily <emily@bacheconsulting.com>
2024-04-12 16:00:26 +0200

    r extract function renderPlainText

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,13 +1,15 @@
 
 function statement (invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
+    return renderPlainText(invoice, plays);
+}
 
+
+function renderPlainText(invoice, plays) {
+    let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
     }
-
-
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
@@ -70,7 +72,7 @@ function statement (invoice, plays) {
         }
         return totalAmount;
     }
-
 }
+
 
 module.exports = statement;
