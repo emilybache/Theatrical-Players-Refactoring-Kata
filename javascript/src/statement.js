a92be39044a58cab4894d4af2cc523a987ad212e
emily <emily@bacheconsulting.com>
2024-04-12 15:45:00 +0200

    R slide statement to put totalAmount next to use

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,15 +1,17 @@
 
 function statement (invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
     }
+
+    let totalAmount = 0;
     for (let perf of invoice.performances) {
         totalAmount += amountFor(perf);
     }
+
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
