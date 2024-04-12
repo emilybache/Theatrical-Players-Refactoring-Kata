a2b5bc4a42ab822b6cf660b6b3e35deb12cf3dfb
emily <emily@bacheconsulting.com>
2024-04-12 15:46:07 +0200

    r inline variable totalAmount

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -8,9 +8,7 @@ function statement (invoice, plays) {
     }
 
 
-    let totalAmount = appleSauce();
-
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(appleSauce())}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
 
