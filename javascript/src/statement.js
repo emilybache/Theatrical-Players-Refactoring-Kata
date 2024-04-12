b5b82137efd386fe80c42fb899962fd713c9985b
emily <emily@bacheconsulting.com>
2024-04-12 16:07:45 +0200

    R introduce enrichPerformance function that takes a copy of the performance

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -2,8 +2,15 @@
 function statement (invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        return result;
+    }
+
+
 }
 
 
