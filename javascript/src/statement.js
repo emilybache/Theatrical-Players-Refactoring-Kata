bc0bdfde99f60f87828f1514f5f7a13a1b51b7b2
emily <emily@bacheconsulting.com>
2024-04-12 14:58:59 +0200

    r inline variable

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -10,14 +10,13 @@ function statement (invoice, plays) {
 
 
     for (let perf of invoice.performances) {
-        const play = playFor(perf);
-        let thisAmount = amountFor(play, perf);
+        let thisAmount = amountFor(playFor(perf), perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
