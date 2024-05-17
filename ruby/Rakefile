f44e4dc1702313ed095225c558d18a3f4b117230
Pablo Alonso García <pabloa@empathy.co>
2024-05-17 17:01:31 +0200

    First implementation in Ruby

---
+++ ruby/Rakefile
@@ -0,0 +1,5 @@
+require "minitest/test_task"
+
+Minitest::TestTask.create # named test, sensible defaults
+
+task :default => :test
---
+++ ruby/test/statement_test.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require 'json'
+require_relative '../lib/theatrical_plays/statement.rb';
+
+class TestStatement < Minitest::Test
+  def test_statement_with_known_types
+    invoice = load_json_fixture('invoice.json')
+    plays = load_json_fixture('plays.json')
+
+    expected_statement = "Statement for BigCo\n" \
+    " Hamlet: $650.00 (55 seats)\n" \
+    " As You Like It: $580.00 (35 seats)\n" \
+    " Othello: $500.00 (40 seats)\n" \
+    "Amount owed is $1730.00\n" \
+    "You earned 40 credits\n"
+
+    assert_equal expected_statement, statement(invoice, plays)
+  end
+
+  def test_statement_with_new_types
+    invoice = load_json_fixture('invoice_new_plays.json')
+    plays = load_json_fixture('new_plays.json')
+
+    error = assert_raises(Exception) do
+      statement(invoice, plays)
+    end
+
+    assert_equal 'unknown type: history', error.message
+  end
+
+  private
+
+  def load_json_fixture(filename)
+    file_path = File.join(__dir__, 'data', filename)
+    JSON.parse(File.read(file_path))
+  end
+end
